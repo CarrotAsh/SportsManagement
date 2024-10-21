24bd60d3af6cba9c72ab60de8e8fd8b9d64b6d64
Wheis <wheisadrian@gmail.com>
2024-10-21 15:18:38 +0200

    create team

--- src/Commands/TeamCreate.java
+++ src/Commands/TeamCreate.java
@@ -1,12 +1,20 @@
 package Commands;
+import Classes.Administrator;
+import Classes.OptionsMenu;
+import Classes.Player;
+import Classes.Team;
 
 public class TeamCreate extends Command{
-    public TeamCreate(String name) {
-        super(name);
+
+    public TeamCreate() {
+        super("team-create");
     }
+
 
     @Override
     public void execute(String[] arguments) {
-
+        arguments = arguments[1].split(";");
+        Team team = new Team;
+        OptionsMenu.team.add(arguments[i]);
     }
 }
--- src/Commands/TeamDelete.java
+++ src/Commands/TeamDelete.java
@@ -1,12 +1,21 @@
 package Commands;
 
+import Classes.Administrator;
+import Classes.OptionsMenu;
+import Classes.Player;
+
 public class TeamDelete extends Command{
-    public TeamDelete(String name) {
-        super(name);
+    public TeamDelete() {
+        super("team-delete");
     }
 
     @Override
     public void execute(String[] arguments) {
-
+        assert OptionsMenu.loggedUser instanceof Administrator;
+        assert arguments.length==2;
+        int i= getTeamPosition(arguments[1]);
+        //assert solo puede borrarse si no está en partido o en equipo en partido
+        OptionsMenu.Team.remove(i);
     }
 }
+
